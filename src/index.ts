1b3acc67b6df7c9a552781fe6f3dfceace0b90ee
Dade Register <Dade.R@microsoft.com>
2022-06-17 20:42:20 -0600

    Missing ;

--- src/index.ts
+++ src/index.ts
@@ -31,7 +31,7 @@ const GlobalVariable = {
                         // Show the installation required modal
                         InstallationRequired.showDialog();
                     }
-                })
+                });
             });
     }
 }
